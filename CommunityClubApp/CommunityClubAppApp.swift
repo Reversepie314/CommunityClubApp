febf5353f9868aaeccd84f085ad48e02dc47df66
Reversepie314 <ka6johnson.kj@gmail.com>
2024-01-29 08:26:09 -0500

    Initial Commit

---
+++ CommunityClubApp/CommunityClubAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  CommunityClubAppApp.swift
+//  CommunityClubApp
+//
+//  Created by Keon Johnson on 1/26/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CommunityClubAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
