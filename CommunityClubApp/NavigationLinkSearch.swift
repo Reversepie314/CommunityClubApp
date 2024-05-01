594e194495d66ae54edec8eebd065e7d72162870
Reversepie314 <ka6johnson.kj@gmail.com>
2024-05-01 09:22:53 -0400

    New

---
+++ CommunityClubApp/NavigationLinkSearch.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationLinkSearch.swift
+//  CommunityClubApp
+//
+//  Created by Keon Johnson on 4/30/24.
+//
+
+import SwiftUI
+
+struct NavigationLinkSearch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationLinkSearch()
+}
---
+++ CommunityClubApp/SearchPageView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchPageView.swift
+//  CommunityClubApp
+//
+//  Created by Keon Johnson on 4/30/24.
+//
+
+import SwiftUI
+
+struct SearchPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchPageView()
+}
